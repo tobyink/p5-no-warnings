f7a22d521c69d6581c20fe5f2ca310d1e5df00b9
Toby Inkster <mail@tobyinkster.co.uk>
2025-03-20 15:05:43 +0000

    init

---
+++ lib/no/warnings.pm
@@ -0,0 +1,50 @@
+use 5.010001;
+use strict;
+use warnings;
+
+package no::warnings;
+
+our $AUTHORITY = 'cpan:TOBYINK';
+our $VERSION   = '0.001000';
+
+1;
+
+__END__
+
+=pod
+
+=encoding utf-8
+
+=head1 NAME
+
+no::warnings - suppress certain warnings for a dynamic scope
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=head1 BUGS
+
+Please report any bugs to
+L<https://github.com/tobyink/p5-no-warnings/issues>.
+
+=head1 SEE ALSO
+
+=head1 AUTHOR
+
+Toby Inkster E<lt>tobyink@cpan.orgE<gt>.
+
+=head1 COPYRIGHT AND LICENCE
+
+This software is copyright (c) 2025 by Toby Inkster.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+
+=head1 DISCLAIMER OF WARRANTIES
+
+THIS PACKAGE IS PROVIDED "AS IS" AND WITHOUT ANY EXPRESS OR IMPLIED
+WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED WARRANTIES OF
+MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+
